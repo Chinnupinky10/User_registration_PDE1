b052c5e4bb0bfc30460f33a067153a0b76af0af9
Chinnupinky10 <thummalapravallika4@gmail.>
2024-01-26 00:00:17 +0530

    first commit

--- project32/settings.py
+++ project32/settings.py
@@ -127,3 +127,11 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='thummalapravallika68@gmail.com'
+EMAIL_HOST_PASSWORD='jlvk xgzl plyb uvpv'
+EMAIL_USE_TLS=True
+EMAIL_USE_SSL=False
+EMAIL_PORT=587
